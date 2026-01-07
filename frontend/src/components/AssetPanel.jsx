287ef26b2bd3929c3a6e4e59d1a8a76f2cb4c67a
Theodore Abshire <TheodoreAbshire@Gmail.com>
2026-01-06 18:55:14 -0800

    Added asset playback, using Shaka Player.

--- frontend/src/components/AssetPanel.jsx
+++ frontend/src/components/AssetPanel.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import Shaka from 'shaka-player'
 
 import assetsService from '../services/assets'
 import CommentDiv from './CommentDiv'
@@ -9,6 +10,29 @@ const AssetPanel = () => {
   const id = useParams().id
   const [asset, setAsset] = useState(null)
   const [comments, setComments] = useState(null)
+  const [player, setPlayer] = useState(new Shaka.Player())
+  const [preload, setPreload] = useState(null)
+  const mediaElement = useRef(null)
+
+  // Start loading the content.
+  useEffect(() => {
+    if (!preload && asset) {
+      player.preload(asset.contentUri).then((manager) => {
+        setPreload(manager)
+      })
+    }
+  }, [asset])
+  // Attach and finish the load. This is a separate effect, so that it doesn't have to happen right away
+  // (e.g. if it's taking a while to load the comments for some reason).
+  useEffect(() => {
+    if (preload && mediaElement && mediaElement.current) {
+      player.attach(mediaElement.current).then(() => {
+        player.load(preload).then(() => {
+          mediaElement.current.play()
+        })
+      })
+    }
+  }, [preload, mediaElement, mediaElement.current])
 
   if (!asset) {
     assetsService.get(id).then(result =>
@@ -23,7 +47,9 @@ const AssetPanel = () => {
     return null
   }
 
-  const getTimestamp = () => 0 // TODO: get timestamp from playback
+  const getTimestamp = () => {
+    return Math.round(mediaElement?.current?.currentTime ?? 0)
+  }
   const addComment = (newComment) => {
     setComments([...comments, newComment])
   }
@@ -31,6 +57,7 @@ const AssetPanel = () => {
   return (
     <div>
       <h4>{asset.name}</h4>
+      <audio controls ref={mediaElement} />
       <list>
         {comments.map(comment =>
           <li id={comment.id}>
