dc13f7a222aba2ac6a2a6f0251ce6a9907fb141d
Theodore Abshire <TheodoreAbshire@Gmail.com>
2026-01-14 00:08:56 -0800

    Also import the CSS styles from Mantine.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { createTheme, MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
 
 import App from './App.jsx'
 import store from './store'
