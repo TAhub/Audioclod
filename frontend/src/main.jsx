c7e406ecb6aa4008e5ed50149971419fd10efb7c
Theodore Abshire <TheodoreAbshire@Gmail.com>
2026-01-13 23:54:43 -0800

    Added Mantine to framework.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { createTheme, MantineProvider } from '@mantine/core'
 
 import App from './App.jsx'
 import store from './store'
 
+const theme = createTheme({
+  // TODO: theme overrides
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <MantineProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MantineProvider>
   </StrictMode>,
 )
