4171db84683567a75dcd36efec2a6b8dfe9eeb90
Theodore Abshire <TheodoreAbshire@Gmail.com>
2026-01-15 01:18:04 -0800

    Added simple avatars to the comment cards.

--- frontend/src/components/CommentDiv.jsx
+++ frontend/src/components/CommentDiv.jsx
@@ -1,14 +1,16 @@
-import { Card, Text, Transition, Group } from '@mantine/core'
+import { Card, Text, Transition, Group, Avatar } from '@mantine/core'
 
 const CommentDiv = ({ comment, visible }) => {
+  const defaultAvatar = comment.user.username.slice(0, 2).toUpperCase()
   return (
     <Transition mounted={visible} transition="fade-right" duration={250} timingFunction="ease" >
       {(transitionStyle) => (
         <Card shadow="sm" padding="lg" radius="md" withBorder style={transitionStyle}>
           <Card.Section withBorder>
             <Group>
+              <Avatar color="blue" radius="xl">{defaultAvatar}</Avatar>
               <Text size="lg" fw={700}>{comment.user.username}</Text>
-              <Text size="lg">at {comment.timestamp}s</Text>
+              <Text size="lg" fs="italic">{comment.timestamp}s</Text>
             </Group>
           </Card.Section>
           <Card.Section>
